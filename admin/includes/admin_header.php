c450659851f39cd6a42f5bb6fb7ceec390324ebd
theaman82 <amankumaraman55555@gmail.com>
2025-01-23 20:23:16 +0530

    admin panel

--- admin/includes/admin_header.php
+++ admin/includes/admin_header.php
@@ -1,1 +1,4 @@
-<!-- admin header work -->
+<div class="flex items-center justify-between bg-[#7469B6] py-3 px-[2%] shadow-lg">
+        <h1 class="text-white text-3xl font-bold">Test Track</h1>
+        <a href="" class="text-lg font-semibold text-white bg-blue-500   border-white rounded px-2 hover:bg-purple-700 py-1">Logout</a>
+    </div>
--- admin/index.php
+++ admin/index.php
@@ -1,1 +1,29 @@
-<!-- All Admin content -->
+<?php
+include_once "../config/connect.php";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Admin Panel</title>
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+<body>
+    <?php include_once "includes/admin_header.php";?>
+    <div>
+        <div class="w-2/12 h-screen flex flex-col gap-3 text-xl font-semibold text-gray-700 bg-[#FFE6E6]">
+            <a href="" class="px-7 py-2 hover:bg-white">Dashboard</a>
+            <a href="" class="px-7 py-2 hover:bg-white">Organise Test</a>
+            <a href="" class="px-7 py-2 hover:bg-white">Manage Exams</a>
+            <a href="" class="px-7 py-2 hover:bg-white">Manage Users</a>
+            <a href="" class="px-7 py-2 hover:bg-white">Manage Students</a>
+            <a href="" class="px-7 py-2 hover:bg-white">Groth Graph</a>
+            <a href="" class="px-7 py-2 hover:bg-white">Settings</a>
+
+        </div>
+        <div class="w-9/12"></div>
+    </div>
+    
+</body>
+</html>
