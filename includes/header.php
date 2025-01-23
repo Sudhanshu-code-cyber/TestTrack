034180c387b430216b60eeb32726557dfe36d8a2
Sudhanshu-code-cyber <sudhanshuaryan2005@gmail.com>
2025-01-23 08:05:45 +0530

    complete header

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,8 @@
-<!-- header content -->
- 1. logo
- 2. navigation common links -> 1.home, 2.contact, 3.about
- 3. External navigation link -> 1.for student in that resources are 1.result, 2.help, 3.my exam
-                                2. for instructot/teacher resources are 1.create exam manage exam
+<div class="flex items-center justify-between bg-[#7469B6] py-4 px-[5%] shadow-lg">
+        <h1 class="text-white text-3xl font-bold">Test Track</h1>
+        <div class="space-x-6">
+            <a href="#" class="text-white text-sm font-medium hover:text-[#FFD700] transition duration-300">Home</a>
+            <a href="#" class="text-white text-sm font-medium hover:text-[#FFD700] transition duration-300">About</a>
+            <a href="#" class="text-white text-sm font-medium hover:text-[#FFD700] transition duration-300">Contact Us</a>
+        </div>
+    </div>
